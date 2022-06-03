92e97d47562d7d26dac621151409456dd8870d71
Hugojuggernot Maître Course <39633298+MrHugojuggernot@users.noreply.github.com>
2022-06-03 04:41:46 +0200

    Reorganisation + starting query-parsing js script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-function revealMessage() {
-    document.getElementById("hiddenMessage").style.display = 'block';
+function f()
+{
+    if (window.location.search.length > 0)
+    {
+        query = window.location.search
+        query = query.replaceAll("?",'')
+        query = query.split("&")
+        if (query.length == 2) // Type (Salade, Plats diverses , sucre , sale) ID =
+        {
+            first= query[0].split("=")
+            second= query[1].split("=")
 
-}
+            if (first in ["salades","autres","sucre","sale"]){
+                console.log(first[1],second[1])
+            }
 
-function countDown() {
-    var currentVal = document.getElementById("countDownButton").innerHTML;
-    if (currentVal > 0){
-        var newVal = currentVal-1;
-        document.getElementById("countDownButton").innerHTML = newVal;
-        document.title = newVal;
+        }
+
 
     }
-
-
-
 }
+
+
+
+
